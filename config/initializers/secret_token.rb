63216e71df23f49174170ce36d50ceb77cf598aa
Freenerd <nospam@freenerd.de>
2013-10-05 12:18:46 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Songaday::Application.config.secret_key_base = 'b456d0bed9ee7857f4d88b6d34fa0087f453bd224b956da05e82e374fdb429c84177b7d58d15da594bb3b96be9407ff4a5c16f7656dadbd31f34ded21e443fd5'
